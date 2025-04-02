455f76282404fd204a40750dc403691b2f51c616
Douglas Gardiner <doug.gardiner@gmail.com>
2025-04-02 06:53:27 -0500

    fixed login issue and added column to products

--- config/database/dps_llc_user_setup.sql
+++ config/database/dps_llc_user_setup.sql
@@ -1,10 +1,22 @@
-create user 'dpsllcuser'@'10.0.0.%' identified by '1989E30BMW325i';
-grant select on *.* to 'dpsllcsuser'@'10.0.0.%';
-grant select, insert, update, delete on dpsllc.* to 'dpsllcuser'@'10.0.0.%';
-grant select, insert, update, delete on dpsllc_pos.* to 'dpsllcuser'@'10.0.0.%';
+drop user 'dpsllcuser'@'10.0.0.%';
+drop user 'dpsllcuser'@'localhost';
+drop user 'posuser'@'10.0.0.%';
+drop user 'posuser'@'localhost';
+flush privileges;
 
+create user 'dpsllcuser'@'10.0.0.%' identified by '1989E30BMW325i';
 create user 'dpsllcuser'@'localhost' identified by '1989E30BMW325i';
+create user 'posuser'@'localhost' identified by '2002E46BMW325i';
+create user 'posuser'@'10.0.0.%' identified by '2002E46BMW325i';
+flush privileges;
+
+grant select on *.* to 'dpsllcsuser'@'10.0.0.%';
 grant select on *.* to 'dpsllcuser'@'localhost';
+grant select, insert, update, delete on dpsllc.* to 'dpsllcuser'@'10.0.0.%';
 grant select, insert, update, delete on dpsllc.* to 'dpsllcuser'@'localhost';
-grant select, insert, update, delete on dpsllc_pos.* to 'dpsllcuser'@'localhost';
+
+grant select on *.* to 'posuser'@'10.0.0.%';
+grant select on *.* to 'posuser'@'localhost';
+grant select, insert, update, delete on dpsllcpos.* to 'posuser'@'10.0.0.%';
+grant select, insert, update, delete on dpsllcpos.* to 'posuser'@'localhost';
 flush privileges;
--- config/database/setup_dps_llc_pos_setup.sql
+++ config/database/setup_dps_llc_pos_setup.sql
@@ -1,5 +1,5 @@
-create database dpsllc_pos;
-use dpsllc_pos;
+create database dpsllcpos;
+use dpsllcpos;
 
 drop table if exists `products`;
 drop table if exists `colors`;
@@ -11,7 +11,8 @@ drop table if exists `orders`;
 create table products (
     id int auto_increment primary key,
     name varchar(40) not null,
-    photo varchar(255)
+    photo varchar(255),
+    url varchar(255)
 ) engine = InnoDB;
 
 create table colors(
@@ -36,6 +37,66 @@ create table states (
     state varchar(20) not null
 ) engine = InnoDB;
 
+insert into states (postal_code, state) values ('AL', 'Alabama');
+insert into states (postal_code, state) values ('AK', 'Alaska');
+insert into states (postal_code, state) values ('AS', 'American Samoa');
+insert into states (postal_code, state) values ('AZ', 'Arizona');
+insert into states (postal_code, state) values ('AR', 'Arkansas');
+insert into states (postal_code, state) values ('CA', 'California');
+insert into states (postal_code, state) values ('CO', 'Colorado');
+insert into states (postal_code, state) values ('CT', 'Connecticut');
+insert into states (postal_code, state) values ('DE', 'Delaware');
+insert into states (postal_code, state) values ('DC', 'District of Columbia');
+insert into states (postal_code, state) values ('FL', 'Florida');
+insert into states (postal_code, state) values ('GA', 'Georgia');
+insert into states (postal_code, state) values ('GU', 'Guam');
+insert into states (postal_code, state) values ('HI', 'Hawaii');
+insert into states (postal_code, state) values ('ID', 'Idaho');
+insert into states (postal_code, state) values ('IL', 'Illinois');
+insert into states (postal_code, state) values ('IN', 'Indiana');
+insert into states (postal_code, state) values ('IA', 'Iowa');
+insert into states (postal_code, state) values ('KS', 'Kansas');
+insert into states (postal_code, state) values ('KY', 'Kentucky');
+insert into states (postal_code, state) values ('LA', 'Louisiana');
+insert into states (postal_code, state) values ('ME', 'Maine');
+insert into states (postal_code, state) values ('MD', 'Maryland');
+insert into states (postal_code, state) values ('MH', 'Marshall Islands');
+insert into states (postal_code, state) values ('MA', 'Massachusetts');
+insert into states (postal_code, state) values ('MI', 'Michigan');
+insert into states (postal_code, state) values ('FM', 'Micronesia');
+insert into states (postal_code, state) values ('MN', 'Minnesota');
+insert into states (postal_code, state) values ('MS', 'Mississippi');
+insert into states (postal_code, state) values ('MO', 'Missouri');
+insert into states (postal_code, state) values ('MT', 'Montana');
+insert into states (postal_code, state) values ('NE', 'Nebraska');
+insert into states (postal_code, state) values ('NV', 'Nevada');
+insert into states (postal_code, state) values ('NH', 'New Hampshire');
+insert into states (postal_code, state) values ('NJ', 'New Jersey');
+insert into states (postal_code, state) values ('NM', 'New Mexico');
+insert into states (postal_code, state) values ('NY', 'New York');
+insert into states (postal_code, state) values ('NC', 'North Carolina');
+insert into states (postal_code, state) values ('ND', 'North Dakota');
+insert into states (postal_code, state) values ('MP', 'Northern Marianas');
+insert into states (postal_code, state) values ('OH', 'Ohio');
+insert into states (postal_code, state) values ('OK', 'Oklahoma');
+insert into states (postal_code, state) values ('OR', 'Oregon');
+insert into states (postal_code, state) values ('PW', 'Palau');
+insert into states (postal_code, state) values ('PA', 'Pennsylvania');
+insert into states (postal_code, state) values ('PR', 'Puerto Rico');
+insert into states (postal_code, state) values ('RI', 'Rhode Island');
+insert into states (postal_code, state) values ('SC', 'South Carolina');
+insert into states (postal_code, state) values ('SD', 'South Dakota');
+insert into states (postal_code, state) values ('TN', 'Tennessee');
+insert into states (postal_code, state) values ('TX', 'Texas');
+insert into states (postal_code, state) values ('UT', 'Utah');
+insert into states (postal_code, state) values ('VT', 'Vermont');
+insert into states (postal_code, state) values ('VA', 'Virginia');
+insert into states (postal_code, state) values ('VI', 'Virgin Islands');
+insert into states (postal_code, state) values ('WA', 'Washington');
+insert into states (postal_code, state) values ('WV', 'West Virginia');
+insert into states (postal_code, state) values ('WI', 'Wisconsin');
+insert into states (postal_code, state) values ('WY', 'Wyoming');
+
 create table customers (
     id int auto_increment primary key,
     fname varchar(40) not null,
@@ -43,7 +104,7 @@ create table customers (
     address1 varchar(100) not null,
     address2 varchar(100),
     city varchar(40) not null,
-    state varchar(2) not null,
+    state varchar(2) not null default "TX",
     foreign key fk_customer_state(state) references states(postal_code),
     zipcode varchar(10) not null
 ) engine = InnoDB;
