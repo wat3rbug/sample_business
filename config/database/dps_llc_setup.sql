5281c831b5add6a439e8f2d78b9db100c72d724e
Douglas Gardiner <doug.gardiner@gmail.com>
2025-04-02 19:40:14 -0500

    fixed expenses so they at least display

--- config/database/dps_llc_setup.sql
+++ config/database/dps_llc_setup.sql
@@ -38,7 +38,7 @@ create table ledgerentries (
 	trans int not null,
 	account int not null,
 	entrytype int not null default 0,
-	amount decimal not null,
+	amount decimal(5, 2) not null,
 	foreign key ledger_entry_transaction_fk(trans) references transactions(id),
 	foreign key ledger_entries_account_fk(account) references accounts(id)
 ) engine = InnoDB;
@@ -49,31 +49,36 @@ create index ledger_entries_account_id_idx on ledgerentries(account);
 create index accounts_account_type_idx on accounts(accounttype);
 	
 create or replace view v_assets as
-	select l.*, a.name from ledgerentries as l
+	select l.*, t.transdate, a.name from ledgerentries as l
+	join transactions as t on l.trans = t.id
 	join accounts as a on l.account = a.id
 	join accounttypes as at on a.accounttype = at.id
-	where at.name = 'Assets';
+	where at.name = 'Assets' order by t.transdate desc;
 	
 create or replace view v_equity as
-	select l.*, a.name from ledgerentries as l
+	select l.*, t.transdate, a.name from ledgerentries as l
+	join transactions as t on l.trans = t.id
 	join accounts as a on l.account = a.id
 	join accounttypes as at on a.accounttype = at.id
-	where at.name = 'Owners Equity';
+	where at.name = 'Owners Equity' order by t.transdate desc;
 	
 create or replace view v_liabilities as
-	select l.*, a.name from ledgerentries as l
+	select l.*, t.transdate, a.name from ledgerentries as l
+	join transactions as t on l.trans = t.id
 	join accounts as a on l.account = a.id
 	join accounttypes as at on a.accounttype = at.id
-	where at.name = 'Liabilities';
+	where at.name = 'Liabilities' order by t.transdate desc;
 	
 create or replace view v_revenue as
-	select l.*, a.name from ledgerentries as l
+	select l.*, t.transdate, a.name from ledgerentries as l
+	join transactions as t on l.trans = t.id
 	join accounts as a on l.account = a.id
 	join accounttypes as at on a.accounttype = at.id
-	where at.name = 'Revenue';
+	where at.name = 'Revenue' order by t.transdate desc;
 	
 create or replace view v_expenses as
-	select l.*, a.name from ledgerentries as l
+	select l.*,t.transdate, a.name from ledgerentries as l
+	join transactions as t on l.trans = t.id
 	join accounts as a on l.account = a.id
 	join accounttypes as at on a.accounttype = at.id
-	where at.name = 'Expenses';
+	where at.name = 'Expenses' order by t.transdate desc;
--- html/js/finance/expenses/expenses.js
+++ html/js/finance/expenses/expenses.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $.ajax({
-        url: "repos/getAllExpenses.php",
+        url: "/repos/getAllExpenses.php",
         dataType: "json",
         success: function(results){
             $('.expenses tbody').empty();
@@ -11,8 +11,33 @@ $(document).ready(function() {
                 line += 'No Data</td></tr>';
                 $('.expenses tbody').append(line);
             } else {
-
+                for (i = 0; i < results.length; i ++) {
+                    var expense = results[i];
+                    var line = '<tr><td>' + getTransDate(expense) + '</td>';
+                    line += '<td>' + expense.name + '</td>';
+                    line += '<td>' + getAmount(expense) + '</td>';
+                    line += '</tr>';
+                    $('.expenses tbody').append(line);
+                }
             }
         }
-    })
+    });
 });
+
+function getTransDate(expense) {
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var trans = new Date(expense.transdate);
+    var year = trans.getUTCFullYear();
+    var month = trans.getUTCMonth();
+    var day = trans.getUTCDay();
+    return day + ' ' + months[month] + ' ' + year;
+}
+
+function getAmount(expense) {
+    const formatter = new Intl.NumberFormat('en-us', {
+        style: "currency",
+        currency: "USD",
+        trailingZeroDisplay: 'stripIfInteger' 
+    });
+    return formatter.format(expense.amount);
+}
